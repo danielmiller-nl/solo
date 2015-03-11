364565473db8371d5ab5e1a3df9ba3d194e622d6
Daniel Miller <danielmiller.nl@gmail.com>
2015-03-10 21:49:21 -0230

    db #6

--- server/server.js
+++ server/server.js
@@ -20,8 +20,8 @@ var wordRequestHandler = require('./textReader.js');
 //   res.mydata.test = "test123456789";
 //   res.send(res.mydata.test);
 // });
-app.use(express.static('./'));
-app.get('./words', wordRequestHandler.requestHandler); 
+app.use(express.static('/'));
+app.get('/words', wordRequestHandler.requestHandler); 
 //app.get('/', requestHandler.requestHandler);
 
 app.listen(process.env.PORT || 3000);
