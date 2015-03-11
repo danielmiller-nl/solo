b0ba0a4b5c4ed411353808178ce4b46c11d7943a
Daniel Miller <danielmiller.nl@gmail.com>
2015-03-10 21:33:39 -0230

    debugging #4

--- server/server.js
+++ server/server.js
@@ -13,15 +13,15 @@
 var express = require('express');
 var app = express();
 var wordRequestHandler = require('./textReader.js');
-console.log("wordRequestHandler",wordRequestHandler);
-app.get('/words', wordRequestHandler.requestHandler);
+
 
 //   function(req, res){
 //   res.mydata = {};
 //   res.mydata.test = "test123456789";
 //   res.send(res.mydata.test);
 // });
-app.use(express.static('./')); 
+app.use(express.static('./'));
+app.get('/words', wordRequestHandler.requestHandler); 
 //app.get('/', requestHandler.requestHandler);
- console.log("Listening on" + process.env.PORT);
+
 app.listen(process.env.PORT || 3000);
